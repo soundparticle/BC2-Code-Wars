3aca6160df508b8c3fd88c84c2022f0085a2ddd7
Antreo Pukay <acrosswalker@yahoo.com>
2018-05-31 13:58:50 -0700

    solution for CW 4

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-function fixTheMeerkat(arr) {
-  var reversed = arr.reverse();
-  return arr;
+function insertDash(num) {
+  let numbers = num.toString().split('');
   
+  for (let i = 0; i < numbers.length; i++) {
+  
+  if (numbers[i] % 2 === 1 && numbers[i + 1] % 2 === 1) {
+    
+        numbers.splice(i + 1, 0, "-");
+  }
  }
+ let newNumber = numbers.join('');
+  return newNumber;
+}
