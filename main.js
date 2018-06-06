69673b3c19dc2c8cb22167f6ee9dd40f0ad3e3fd
Antreo Pukay <acrosswalker@yahoo.com>
2018-06-05 22:28:56 -0700

    cw-7a solution

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-
-
-var replaceDots = function(str) {
-    return str.replace(/\./g, '-');
-  }
+function validatePIN (pin) {
+  if(pin.length === 4 ||  pin.length === 6 ) {
+    if(/^(\d{4}|\d{6})$/.test(pin))  {
+      return true;
+    }else {return false;}
+  }else {
+      return false;
+      }
+}
